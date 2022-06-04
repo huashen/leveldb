e0f93131c0b2870c48b7750422662411427f59e0
huashen <huashen361@gmail.com>
2022-06-04 23:33:29 +0800

    db分析

--- db/db_impl.cc
+++ db/db_impl.cc
@@ -1483,14 +1483,17 @@ DB::~DB() = default;
 Status DB::Open(const Options& options, const std::string& dbname, DB** dbptr) {
   *dbptr = nullptr;
 
+  //初始化dbImpl
   DBImpl* impl = new DBImpl(options, dbname);
   impl->mutex_.Lock();
   VersionEdit edit;
   // Recover handles create_if_missing, error_if_exists
   bool save_manifest = false;
+  //尝试恢复之前已经存在的数据库文件中的数据
   Status s = impl->Recover(&edit, &save_manifest);
   if (s.ok() && impl->mem_ == nullptr) {
     // Create new log and a corresponding memtable.
+    //创建新的Log和MemTable
     uint64_t new_log_number = impl->versions_->NewFileNumber();
     WritableFile* lfile;
     s = options.env->NewWritableFile(LogFileName(dbname, new_log_number),
